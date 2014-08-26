df7e0c4f65b5797e4031f9f75950acd07bbbd8fa
Mikhail Spivakov <mikhail.spivakov@babraham.ac.uk>
2014-08-26 12:05:24 +0100

    Initial commit

---
+++ Test.R
@@ -0,0 +1,70 @@
+#Get code:
+setwd("~/CHiCAGOtest/")
+source("chicago.R")
+
+#Overwrite file locations
+fileDir = "/bi/group/sysgen/CHIC"
+rmapfile= file.path(fileDir, "Digest_Human_HindIII.bed")
+baitmapfile= file.path(fileDir, "Digest_Human_HindIII_baits_ID.bed")
+nperbinfile = file.path(fileDir, "Digest_Human_HindIII_NperBin.txt")
+nbaitsperbinfile = file.path(fileDir, "Digest_Human_HindIII_NbaitsPerBin.txt")
+proxOEfile = file.path(fileDir, "proxOE_out.txt")
+
+
+#Choose data to read in:
+dataFileDir <- "/bi/group/sysgen/hESC"
+datafile <- file.path(dataFileDir, "sample_hESC811_new/hESC811_new_bait_otherEnd_N_len_distSign.txt")
+peakfile <- file.path(dataFileDir, "res_hESC_811/data/hESC_811.RData")
+
+
+#Run CHiCAGO on data:
+
+x <- readSample(datafile)
+gc()
+x <- normaliseBaits(x)
+gc()
+x <- normaliseOtherEnds(x)
+gc()
+f <- estimateDistFun(x)
+gc()
+x <- estimateTechnicalNoise(x)
+gc()
+#x <- estimateBrownianNoise(x, f) ##all
+x <- estimateBrownianNoise(x, f, subset=1000)
+gc()
+x <- getPvals(x)
+gc()
+x <- getFDRs(x)
+gc()
+##FIXME call interactions
+
+#save output somewhere...
+save(x, f, file="results.Rda")
+
+##All of the below was put in TestReport.Rmd
+
+# ##a quick test (to make sure the table writes correctly)----------------
+# #testfribble <- x[1:10,]
+# #write.csv(, "")
+# 
+# ##compare against v1 output---------------------------------------------
+# load(peakfile)
+# 
+# #baitinfo <- fread(baitmapfile)
+# #fraginfo <- fread(rmapfile)
+# 
+# ##combine previous results into a nice(r) format
+# cols <- c("baitID", "otherEndID", "isBait2bait")
+# n1cat <- rbind(
+#   as.data.table(n1pr[,c(cols,"logwp")]),
+#   as.data.table(n1farpr[,c(cols,"logwp_bw")]),
+#   use.names=FALSE ##because the colnames differ
+#   )
+# setnames(n1cat, c(cols,"logwp"))
+# 
+# x.sel <- as.data.table(x[,c("baitID", "otherEndID", "log.q")])
+# 
+# setkey(n1cat, baitID, otherEndID)
+# setkey(x.sel, baitID, otherEndID)
+# 
+# venn(list(unique(x.sel$baitID), unique(n1cat$baitID)))
---
+++ process_hicup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [ $# -lt 3 ]
+  then
+    echo "Usage: process_hicup.sh <bamfile> <bait-fragments-with-id-file> <digest-bait-with-id-file> [<output-dir>]"
+    exit
+fi
+
+bam=$1
+bamname=`echo ${bam%\.*}`
+
+baitfendsid=$2
+digestbed=$3
+
+outputdir=.
+
+if [ $# -gt 3 ]
+  then
+    outputdir=$4
+fi
+
+#echo "Converting the input bam file ${bam} to a paired-end bed file ${bamname}.bedpe..."
+#bedtools bamtobed -bedpe -i $bam > ${outputdir}/${bamname}.bedpe
+
+baitcolno=`awk '{print NF; exit}' ${baitfendsid}`
+
+if [ $baitcolno -gt 4 ]
+  then
+    echo "Baitfendsid file contains >4 columns. Probably means that full bait restriction fragment map was provided instead of valid fends, but it's ok for HiCUP output at least. Trimming the extra columns and saving the result in ${baitfendsid}_4col.txt..."
+    awk '{print $1"\t"$2"\t"$3"\t"$4}' ${baitfendsid} > ${baitfendsid}_4col.txt
+    baitfendsid=`echo ${baitfendsid}_4col.txt`
+fi
+
+echo "Intersecting with bait fragments (using min overhang of 0.6)..."
+#bedtools pairtobed -a ${outputdir}/${bamname}.bedpe -b $baitfendsid -f 0.6 > ${outputdir}/${bamname}_mappedToBaits.bedpe
+bedtools pairtobed -abam $bam -bedpe -b $baitfendsid -f 0.6 > ${outputdir}/${bamname}_mappedToBaits.bedpe
+
+echo "Flipping all reads that overlap with the bait on to the right-hand side..."
+#awk 'BEGIN{OFS="\t"}{if($1==$11 && $3>=$12 && $2<=$13){print $4,$5,$6,$1,$2,$3,$7,$8,$10,$9,$11,$12,$13,$14,$15}else{print $0}}' ${outputdir}/${bamname}_mappedToBaits.bedpe > ${outputdir}/${bamname}_mappedToBaits_baitOnRight.bedpe
+awk 'BEGIN{OFS="\t"}{minRight=$13<$3?$13:$3; maxLeft=$12>$2?$12:$2; if($1==$11 && (minRight-maxLeft)/($3-$2)>=0.6){print $4,$5,$6,$1,$2,$3,$7,$8,$10,$9,$11,$12,$13,$14,$15}else{print $0}}' ${outputdir}/${bamname}_mappedToBaits.bedpe > ${outputdir}/${bamname}_mappedToBaits_baitOnRight.bedpe
+
+echo "Intersecting with bait fragments again to produce a list of bait-to-bait interactions that can be used separately; note they will also be retained in the main output..."
+bedtools intersect -a ${outputdir}/${bamname}_mappedToBaits_baitOnRight.bedpe -wo -f 0.6 -b $baitfendsid > ${outputdir}/${bamname}_bait2bait.bedpe
+
+#echo "Removing reads that failed the min overhang filter..."
+#awk '{if ($0!~/\-1\t\-1/){print $0}}' ${outputdir}/${bamname}_bait2bait.bedpe > ${outputdir}/${bamname}_bait2bait_fmore06.bedpe
+
+echo "Intersecting with restriction fragments (using min overhang of 0.6)..."
+bedtools intersect -a ${outputdir}/${bamname}_mappedToBaits_baitOnRight.bedpe -wao -f 0.6 -b $digestbed > ${outputdir}/${bamname}_mappedToBaitsBoRAndRFrag.bedpe
+
+echo "Removing reads that failed the min overhang filter (saving separately into ${bamname}_mappedToBaitsBoRAndRFrag_fless06.bedpe)..."
+awk -v fless=${outputdir}/${bamname}_mappedToBaitsBoRAndRFrag_fless06.bedpe '{if ($0~/\-1\t\-1/){print $0 > fless} else {print $0}}' ${outputdir}/${bamname}_mappedToBaitsBoRAndRFrag.bedpe > ${outputdir}/${bamname}_mappedToBaitsBoRAndRFrag_fmore06.bedpe
+
+echo "Adding frag length and signed distance from bait; removing self-ligation fragments (if any; not expected with HiCUP input)..."
+
+perl -ne '{ chomp $_; my @a = split /\t/, $_; my $l = $a[16]-$a[15]; my $d = "NA"; if ($a[10] eq $a[14]){ my $midB = $a[11]+int(($a[12]-$a[11])/2+0.5); my $midR = $a[15]+int(($a[16]-$a[15])/2+0.5); $d = $midR - $midB; } if ($d ne "0") { print "$_\t$l\t$d\n" }}' ${outputdir}/${bamname}_mappedToBaitsBoRAndRFrag_fmore06.bedpe > ${outputdir}/${bamname}_mappedToBaitsBoRAndRFrag_fmore06_withDistSignLen.bedpe
+
+echo "The file with reads aligned to baits and other ends is in the ${outputdir} folder and is called ${bamname}_mappedToBaitsBoRAndRFrag_fmore06_withDistSignLen.bedpe"
