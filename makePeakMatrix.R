d6243f030f942a42563fa4a7d6aab69a77c27a37
Mikhail Spivakov <mikhail.spivakov@babraham.ac.uk>
2015-03-26 11:06:01 +0000

    Switched to using data tables to save memory

--- makePeakMatrix.R
+++ makePeakMatrix.R
@@ -1,4 +1,7 @@
+library(data.table)
+library(matrixStats)
 library(argparser)
+
 cat("\n")
 args = commandArgs(trailingOnly=T)
 spec = matrix(c("<names-file>", "Full path to a tab-separated file with sample names (1st column) and full paths to input Rda files (2nd column)", 
@@ -34,23 +37,11 @@ for (i in 1:nrow(input)){
 
      cat("Processing",  f, "...")
      x = x[!is.na(x$distSign),]
-     
-#      t1 = gsub("step2_data_processed_separately/res_\\S+_chicago2/data/", "", f)
-#      t2 = gsub("_step2.RDa", "", t1)
-  
+       
      name = input[i, "name"]
      data[[name]] = x[, c("baitID", "otherEndID", "score")]
      cat("done\n")    
 }
-
-# setwd(currdir)
-
-#save(data, file="data.Rda")
-
-library(data.table)
-library(matrixStats)
-#cat("Loading data...\n")
-#load("data.Rda")
 
 cat("Converting to data.table and indexing...\n")
 for (d in names(data)){
@@ -63,17 +54,18 @@ cat("Merging...\n")
 z = Reduce(function(x,y) merge(x,y,all=TRUE), data)
 
 cat("Retaining only interactions exceeding score cutoff", cutoff, "in at least one sample...\n")
-z = as.data.frame(z)[rowMaxs(as.data.frame(z)[,3:ncol(z)], na.rm = T)>=cutoff ,]
+scoreCols = names(z)[3:ncol(z)]
+z[, rmax:=eval(parse(text=paste0("pmax(", paste0(scoreCols, collapse=","),", na.rm=T)")))]
+z = z[rmax>=cutoff]
 
 cat("Replacing NAs and negative scores with zeros...\n")
 for (i in 3:ncol(z)){
-   z[is.na(z[,i]) | z[,i]<0, i] = 0
+   set(z, which(is.na(z[[i]]) | z[[i]]<0), i, 0)
 }
 
-
 cat("Adding bait coordinates and annotation...\n")
-z = data.table(z)
 setkey(z, baitID)
+
 bm = fread(baitmapfile)
 setnames(bm, c("V1", "V2", "V3", "V4", "V5"), c("baitChr", "baitStart", "baitEnd", "baitID", "baitName"))
 
@@ -97,16 +89,16 @@ cat("Annotating bait2bait interations...\n")
 setnames(bm, c("baitID", "baitName"), c("otherEndID", "otherEndName")) # sic!
 setkey(bm, otherEndID)
 z = merge(z, bm[, c("otherEndID", "otherEndName"), with=F], all.x=T, all.y=F)
-z$otherEndName[is.na(z$otherEndName)] = "."
+set(z, which(is.na(z$otherEndName)), "otherEndName", ".")
 
 cat("Reordering and sorting...\n")
 
-z$dist = NA_real_
-z[baitChr==oeChr]$dist = with(z[baitChr==oeChr], ceiling(oeStart+(oeEnd-oeStart)/2-(baitStart+(baitEnd-baitStart)/2))) 
+z[, dist := NA_real_]
+z[baitChr==oeChr, dist := ceiling(oeStart+(oeEnd-oeStart)/2-(baitStart+(baitEnd-baitStart)/2))]
 
 setnames(z, "otherEndID", "oeID")
 setnames(z, "otherEndName", "oeName")
-z = as.data.frame(z)
+setDF(z)
 z = z[, c("baitChr", "baitStart", "baitEnd", "baitID", "baitName", "oeChr", "oeStart", "oeEnd", "oeID", "oeName", "dist", input[,"name"])]
 
 z = z[order(z$baitChr, z$baitStart, z$oeChr, z$oeStart), ]
