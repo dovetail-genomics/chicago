93207311d81468b00bf8af7bba18663a70ec9b55
Mikhail Spivakov <mikhail.spivakov@babraham.ac.uk>
2015-04-21 15:46:38 +0100

    switched to platform-independent file system operations in runChicago

--- runChicago.R
+++ runChicago.R
@@ -1,6 +1,39 @@
 library(argparser)
 library(Chicago)
 
+dir.create.ifNotThere = function(path, ...){
+  if(file.exists(path)){
+    if(!file.info(path)$isdir){
+      message("Cannot create directory ", path, " as it coincides with an existing file\n")
+      return(0)
+    }else{
+      message("\nWarning: directory ", path, " exists and will be reused.")
+      return(1)
+    }
+  }
+  else{
+    dir.create(path, ...)
+  }
+}
+
+moveToFolder = function(pattern, where){
+  files = list.files(".", pattern)
+  if (!length(files)){
+    message("No files found matching the pattern ", pattern)
+    return(0)
+  }
+  res = vector("numeric", length(files))
+  i=1
+  for(f in files){
+    res[i] = file.rename(f, file.path(where, f))
+    if (!res[i]){
+      message("Warning: did not succeed in moving ", f, " into ", where)
+    }
+    i = i+1
+  }
+  min(res)
+}
+
 args = commandArgs(trailingOnly=T)
 spec = matrix(c("<input-files>", "Full path to the input file (or comma-separated list of files)", 
                 "<output-prefix>", "Output file names prefix (cannot contain folders)"),  byrow=T, ncol=2)
@@ -16,8 +49,8 @@ p = add.argument(p, arg="--print-memory", help = "Should chicagoPipeline print o
 
 p = add.argument(p, arg="--cutoff", help = "Score cutoff for writing out peaks and testing feature enrichment", default = 5)
 p = add.argument(p, arg="--export-format", 
-                 help = "File format for writing out peaks: one or more of the following: seqMonk,interBed,washU (comma-separated)", 
-                 default = "washU")
+                 help = "File format for writing out peaks: one or more of the following: seqMonk,interBed,washU_text,washU_track (comma-separated)", 
+                 default = "washU_text")
 p = add.argument(p, arg="--export-order", help = "Should the results be ordered by \"score\" or genomic \"position\"?", 
                  default = "position")
 
@@ -40,7 +73,7 @@ p = add.argument(p, arg="--output-dir", help = "The name of the output directory
 opts = parse.args(p, args)
 
 inputFiles = strsplit(opts[["<input-files>"]], "\\,")[[1]]
-outPrefix = opts[["<output-prefix>"]]
+outPrefix_rel = opts[["<output-prefix>"]]
 
 settingsFile = opts[["settings-file"]]
 designDir = opts[["design-dir"]]
@@ -62,14 +95,16 @@ plotFull = opts[["examples-full-range"]]
 
 featDistLim = opts[["feat-max-dist"]]
 
-outDir = ifelse(opts[["output-dir"]]=="<output-prefix>", outPrefix, opts[["output-prefix"]])
+outDir = ifelse(opts[["output-dir"]]=="<output-prefix>", outPrefix_rel, opts[["output-dir"]])
+
+outPrefix = file.path(outDir, outPrefix_rel)
 
 if(is.na(settingsFile)){
   settingsFile = NULL
 }
 
-if (!all(exportFormat %in% c("seqMonk", "interBed", "washU"))) {
-  stop("--exportFormat must be either seqMonk, interBed or washU (or a comma-separated combination of these)\n")
+if (!all(exportFormat %in% c("seqMonk", "interBed", "washU_text", "washU_track"))) {
+  stop("--exportFormat must be either seqMonk, interBed, washU_text or washU_track (or a comma-separated combination of these)\n")
 }
 if (!exportOrder %in% c("position", "score")) {
   stop("--exportOrder must be either position (default) or score\n")
@@ -80,18 +115,17 @@ cd = setExperiment(designDir = designDir, settingsFile = settingsFile)
 
 
 if(length(inputFiles)>1){
-  message("Reading input files...\n")
+  message("\nReading input files...\n")
   cd = readAndMerge(inputFiles, cd)
 }else{
+  message("\n")
   cd = readSample(inputFiles, cd)
 }
 
-if(system(paste("mkdir -p", outDir))){
+if(!dir.create.ifNotThere(outDir, recursive = T)){
   stop(paste("Couldn't create folder", outDir, "\n"))
 }
 
-curDir = getwd()
-setwd(outDir)
 
 message("\n\nStarting chicagoPipeline...\n")
 cd = chicagoPipeline(cd, outprefix = outPrefix, printMemory = printMemory)
@@ -124,33 +158,53 @@ message("\n\nExporting peak lists...\n")
 exportResults(cd, outfileprefix=outPrefix, cutoff=cutoff, format = exportFormat, order = exportOrder)
 
 message("\n\nSorting output files into folders...\n")
-if(system("mkdir -p data")){
+
+curDir = getwd()
+setwd(outDir)
+
+if(!dir.create.ifNotThere("data")){
   stop("Couldn't create folder data\n")  
 }
-if(system("mkdir -p diag_plots")){
+if(!dir.create.ifNotThere("diag_plots")){
   stop("Couldn't create folder diag_plots\n")
 }
-if(system("mkdir -p examples")){
+if(!dir.create.ifNotThere("examples")){
   stop("Couldn't create folder examples\n")      
 }
-if(system("mkdir -p enrichment-data")){
+if(!dir.create.ifNotThere("enrichment-data")){
   stop("Couldn't create folder enrichment-data\n")      
 }
 
-system("mv *.txt data/")
+### TODO: if bgzip and tabix index files are going to be produced in washU-track mode, move them too. 
+if (!moveToFolder("\\.txt", "data")){
+  stop("Couldn't move txt files to data folder\n")
+}
 
 if("interBed" %in% exportFormat){
-  system("mv *.ibed data/")
+  if(!moveToFolder("\\.ibed", "data")){
+    stop("Couldn't move ibed files to data folder\n")
+  }
 }
 
 if(isRda){
-  system("mv *.RDa data/")
+  if(!moveToFolder("\\.RDa", "data")){
+    stop("Couldn't move the RDa file to data folder\n")
+  }
 }else{
-  system("mv *.Rds data/")  
+  if(!moveToFolder("\\.Rds", "data")){
+    stop("Couldn't move the Rds file to data folder\n")
+  }
 }
 
-system("mv *xamples.pdf examples/")
-system("mv *.pdf diag_plots/")       
+if(!moveToFolder("xamples\\.pdf", "examples")){
+  stop("Couldn't move pdf file(s) to example folder\n")
+}
+
+if(!moveToFolder("\\.pdf", "diag_plots")){
+  stop("Couldn't move pdf files to diag_plots folder\n")
+}
+
+setwd(curDir)
 
 if (!is.na(featureFiles)[1] | !is.na(featureList)){
   message("\n\nComputing enrichment for features...\n")
@@ -162,8 +216,10 @@ if (!is.na(featureFiles)[1] | !is.na(featureList)){
 #                           plot_name=paste0(outprefix, "_feature_overlaps_upto_1M.pdf"))
 #   
 #   write.table(resTable, quote=F, row.names= , col.names=T, file=   paste0(outprefix, "_feature_overlaps_upto1M.txt")) 
-#   system("mv *feature_overlaps*.* enrichment-data/")
+#   setwd(outDir)
+#   if(!moveToFolder("feature_overlaps", "enrichment")){
+#     stop("Couldn't move feature_overlaps files to enrichment folder")
+#   }
 }
-
 setwd(curDir)
 message("All done!\n")
