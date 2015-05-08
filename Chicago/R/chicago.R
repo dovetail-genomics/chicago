ef00d383a00990bcff91efb0a2625cd12cf1d693
Jonathan Cairns <jonathan.cairns@babraham.ac.uk>
2015-05-08 14:19:03 +0100

    Altered estimateDistFun, added .distFun. Pipeline broken by this change...

--- Chicago/R/chicago.R
+++ Chicago/R/chicago.R
@@ -543,109 +543,124 @@ normaliseOtherEnds = function(cd, Ncol="NNb", normNcol="NNboe", plot=TRUE, outfi
 
 }
 
-### I've modified this function so it updates cd@params$f and returns cd.
-### This way f will be retained along with other params such as s_k and dispersion.
-### Just need to make sure cd@x doesn't get copied when we do this... 
-estimateDistFun <- function (cd, method="cubic", n.obs.head=10, n.obs.tail=25, logScale=FALSE, plot=TRUE, outfile=NULL) {
+##updates cd@params$distFunParams, can then be fed into .distFun()
+estimateDistFun <- function (cd, method="cubic", plot=TRUE, outfile=NULL) {
   
   # Take the "refBinMean" column of the data x as f(d_b)
   # then interpolate & extrapolate to get f(d).
   # TODO output extra diagnostic information?
-  # TODO optimize with data.table
   
-  if (!method %in% c("lm", "cubic")){
+  if (!method == "cubic"){
     stop ("Unknown method.\n")
   }
   
   # Get f(d_b)
-#   f.d <- unique(x[!is.na(x$refBinMean),c("distbin", "refBinMean")]) ##delete rows with NAs from baits that are too far away
+  #   f.d <- unique(x[!is.na(x$refBinMean),c("distbin", "refBinMean")]) ##delete rows with NAs from baits that are too far away
   setkey(cd@x, distbin, refBinMean)
   f.d <- unique(cd@x)[is.na(refBinMean)==FALSE][, c("distbin", "refBinMean"), with=F]
   
-#   f.d <- f.d[order(f.d$refBinMean, decreasing=TRUE),]
+  #   f.d <- f.d[order(f.d$refBinMean, decreasing=TRUE),]
   f.d <- f.d[order(refBinMean, decreasing=TRUE)]
-
+  
   setDF(f.d) # f.d is tiny, so no need to bother with it being a data.table
   f.d$midpoint <- seq(from=round(cd@settings$binsize/2), by=cd@settings$binsize, length.out=nrow(f.d))
   
   obs.min <- log(min(f.d$midpoint))
   obs.max <- log(max(f.d$midpoint))
   
-  if(method == "lm") {
-    ##On log-scale, do a linear interpolation.
-    ##Linear models applied to first (n.obs.head) observations, and to last (n.obs.tail) observations.
-    
-    ##Interpolation: Estimate f(d) (NB "rule" parameter = 1 forces NAs outside of range)
-    log.f.obs <- approxfun(log(f.d$midpoint), log(f.d$refBinMean), rule=c(1,1))
-    
-    ##Extrapolation: Fit the "head" and "tail" of f using a linear model
-    head.coef <- coefficients(lm(log(refBinMean)~log(midpoint), data = head(f.d, n.obs.head))) ##Fit for small d
-    tail.coef <- coefficients(lm(log(refBinMean)~log(midpoint), data = tail(f.d, n.obs.tail))) ##Fit for large d
-    
-    log.f.head <- function(x, head.coef.=head.coef) {head.coef.[1] + x*head.coef.[2]}
-    log.f.tail <- function(x, tail.coef.=tail.coef) {tail.coef.[1] + x*tail.coef.[2]} ##explicitly stated in case of later change
-    
-  }
+  #   if(method == "lm") {
+  #
+  #     ##previously had arguments: n.obs.head=10, n.obs.tail=25
+  #
+  #     ##On log-scale, do a linear interpolation.
+  #     ##Linear models applied to first (n.obs.head) observations, and to last (n.obs.tail) observations.
+  #     
+  #     ##Interpolation: Estimate f(d) (NB "rule" parameter = 1 forces NAs outside of range)
+  #     log.f.obs <- approxfun(log(f.d$midpoint), log(f.d$refBinMean), rule=c(1,1))
+  #     
+  #     ##Extrapolation: Fit the "head" and "tail" of f using a linear model
+  #     head.coef <- coefficients(lm(log(refBinMean)~log(midpoint), data = head(f.d, n.obs.head))) ##Fit for small d
+  #     tail.coef <- coefficients(lm(log(refBinMean)~log(midpoint), data = tail(f.d, n.obs.tail))) ##Fit for large d
+  #     
+  #   }
   
   if(method == "cubic") {
     ##Spline - Cubic fit over observed interval, linear fit elsewhere, assume continuity of f(d) & f'(d).
+    distFunParams <- list(method="cubic")
     
     ##cubic fit (quadratic not immensely different TBH)
     f.d.cubic <- lm(log(refBinMean) ~ log(midpoint) + I(log(midpoint)^2) + I(log(midpoint)^3), data = f.d)
     fit <- f.d.cubic$coefficients
-    
-    ##Interpolation: Estimate f(d) from cubic (NB see "rule" parameter for what to do outside range)
-    log.f.obs <- function(x, fit. = fit) {fit.[1] + fit.[2]*x + fit.[3]*(x^2) + fit.[4]*(x^3)}
+    distFunParams[["cubicFit"]] <- fit
     
     ##Extrapolation: Fit the "head" and "tail" of f using continuity
-    obs.min <- log(min(f.d$midpoint))
-    obs.max <- log(max(f.d$midpoint))
+    distFunParams[["obs.min"]] <- log(min(f.d$midpoint))
+    distFunParams[["obs.max"]] <- log(max(f.d$midpoint))
     
     beta <- fit[2] + 2*fit[3]*c(obs.min, obs.max) + 3*fit[4]*(c(obs.min, obs.max)^2)
     alpha <- fit[1] + (fit[2] - beta)*c(obs.min, obs.max) + fit[3]*c(obs.min, obs.max)^2 + fit[4]*c(obs.min, obs.max)^3
     
-    head.coef <- c(alpha[1], beta[1])
-    tail.coef <- c(alpha[2], beta[2])
-    
-    log.f.head <- function(x, head.coef.=head.coef) {head.coef.[1] + x*head.coef.[2]}
-    log.f.tail <- function(x, tail.coef.=tail.coef) {tail.coef.[1] + x*tail.coef.[2]} ##explicitly stated in case of later change
+    distFunParams[["head.coef"]] <- c(alpha[1], beta[1])
+    distFunParams[["tail.coef"]] <- c(alpha[2], beta[2])
     
   }
   
-  ##Put everything together to get the final function
-  ##All these appended dots (e.g. "head.conf.") are to avoid errors of form "promise already under evaluation"
-  log.f <- function(x, head.coef.=head.coef, tail.coef.=tail.coef, obs.min.=obs.min, obs.max.=obs.max)
-  {
-    ifelse(x > obs.max.,
-           log.f.tail(x, tail.coef.), ##Common case evaluated first
-           ifelse(x < obs.min.,
-                  log.f.head(x, head.coef.),
-                  log.f.obs(x))
-    )
-  }
-  if(logScale)
-  {
-    f <- log.f
-  } else {
-    f <- function(x) exp(log.f(log(x)))
-  }
+  
   if(plot)
   {
     if (!is.null(outfile)){ 
       pdf(outfile)
     }
-      curve(log.f.obs, obs.min, obs.max,
-            main = paste0("Distance function (points = obs, line = ", method, " fit)"),
-            xlab = "log(distance)",
-            ylab = "log(f(d))")
-      with(f.d, points(log(midpoint), log(refBinMean)))
-    if (!is.null(outfile)){ 
+    my.log.d <- seq(from=obs.min, to=obs.max, length.out = 101)
+    my.d <- exp(my.log.d)
+    plot(my.log.d, log(.distFun(my.d, distFunParams)),
+         type="l",
+         main = paste0("Distance function (points = obs, line = ", method, " fit)"),
+         xlab = "log(distance)",
+         ylab = "log(f(d))",
+         col = "Red")
+    with(f.d, points(log(midpoint), log(refBinMean)))
+    if (!is.null(outfile)){
       dev.off()
     }
   }
-
-  cd@params$f = f
+  
+  cd@params$distFunParams <- distFunParams
   cd
+}
+
+.distFun <- function(d, distFunParams)
+{
+  ##d: distSign column of cd@x
+  
+  ##distFunParams: list of form
+  ##list(method, ...)
+  ##so list("cubic", head.coef, tail.coef, obs.min, obs.max, fit)
+  ##in future, could support e.g. list("lm", ...)
+  
+  p <- distFunParams
+  
+  stopifnot(p[["method"]] == c("cubic")) ##compatibility with future "method"s
+  
+  ##Transfer parameters
+  obs.max <- p[["obs.max"]]
+  obs.min <- p[["obs.min"]]  
+  head.coef <- p[["head.coef"]]
+  tail.coef <- p[["tail.coef"]]
+  fit <- p[["cubicFit"]]
+  
+  ##Put everything together to get the final function
+  d <- log(d)
+  
+  out <- ifelse(d > obs.max,
+                tail.coef[1] + d*tail.coef[2], ##Common case evaluated first (large d)
+                ifelse(d < obs.min,
+                       head.coef[1] + d*head.coef[2],
+                       fit[1] + fit[2]*d + fit[3]*(d^2) + fit[4]*(d^3) ##2nd most common case evaluated second (small d)
+                )
+  )
+  
+  exp(out)
 }
 
 estimateBrownianNoise <- function(cd) {
