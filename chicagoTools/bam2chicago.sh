575ba24dd63411147661dc02de4b559b4cdfc658
Mikhail Spivakov <mikhail.spivakov@babraham.ac.uk>
2015-05-22 11:22:29 +0100

    put chicagoTools under version control

---
+++ chicagoTools/bam2chicago.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -lt 4 ]
+  then
+    echo "Usage: bam2chicago.sh <bamfile> <baitmap-file> <digest-rmap-file> <sample-name> [nodelete]"
+    exit
+fi
+
+command -v bedtools >/dev/null 2>&1 || { echo >&2 "Error: Cannot execute bedtools. Check that it's installed and added to PATH. Aborting."; exit 1; }
+command -v awk >/dev/null 2>&1 || { echo >&2 "Error: Cannot execute awk. Aborting."; exit 1; }
+command -v perl >/dev/null 2>&1 || { echo >&2 "Error: Cannot execute perl. Aborting."; exit 1; }
+
+#SCRIPT=$(readlink -f $0)
+#pipelinedir=`dirname $SCRIPT`
+
+bam=$1
+bam0=`basename ${bam}`
+bamname=`echo ${bam0%\.*}`
+baitfendsid=$2
+digestbed=$3
+samplename=$4
+nodelete=$5
+
+mkdir -p ${samplename}
+
+echo "Processing sample ${samplename}..."
+echo "Using bam file ${bam}"
+echo "Using baitmap file ${baitfendsid}"
+echo "Using digest map (rmap) file ${digestbed}"
+
+baitcolno=`awk '{print NF; exit}' ${baitfendsid}`
+
+if [ $baitcolno -gt 4 ]; then
+    echo "Baitmap file contains >4 columns. Checking if ${baitfendsid}_4col.txt exists..."
+    if ! [ -e ${baitfendsid}_4col.txt ]; then
+	   echo "It doesn't. So trimming the extra columns and saving the result in ${baitfendsid}_4col.txt..."
+           awk '{print $1"\t"$2"\t"$3"\t"$4}' ${baitfendsid} > ${baitfendsid}_4col.txt
+    else
+           echo "Found ${baitfendsid}_4col.txt"
+    fi
+    baitfendsid=`echo ${baitfendsid}_4col.txt`
+fi
+
+echo "Intersecting with bait fragments (using min overhang of 0.6)..."
+bedtools pairtobed -abam $bam -bedpe -b $baitfendsid -f 0.6 > ${samplename}/${bamname}_mappedToBaits.bedpe
+
+echo "Flipping all reads that overlap with the bait on to the right-hand side..."
+awk 'BEGIN{ OFS="\t" }
+     {
+     minRight=$13<$3?$13:$3; 
+     maxLeft=$12>$2?$12:$2; 
+     if($1==$11 && (minRight-maxLeft)/($3-$2)>=0.6){
+              print $4,$5,$6,$1,$2,$3,$7,$8,$10,$9,$11,$12,$13,$14,$15
+     }
+     else {
+              print $0
+     }
+}' ${samplename}/${bamname}_mappedToBaits.bedpe > ${samplename}/${bamname}_mappedToBaits_baitOnRight.bedpe
+
+if [ "$nodelete" != "nodelete" ]; then
+	rm ${samplename}/${bamname}_mappedToBaits.bedpe
+fi
+
+echo "Intersecting with bait fragments again to produce a list of bait-to-bait interactions that can be used separately; note they will also be retained in the main output..."
+echo "##	samplename=${samplename}	bamname=${bamname}	baitmapfile=${baitfendsid}	digestfile=${digestbed}" > ${samplename}/${samplename}_bait2bait.bedpe
+bedtools intersect -a ${samplename}/${bamname}_mappedToBaits_baitOnRight.bedpe -wo -f 0.6 -b $baitfendsid >> ${samplename}/${samplename}_bait2bait.bedpe
+
+echo "Intersecting with restriction fragments (using min overhang of 0.6)..."
+bedtools intersect -a ${samplename}/${bamname}_mappedToBaits_baitOnRight.bedpe -wao -f 0.6 -b $digestbed > ${samplename}/${bamname}_mappedToBaitsBoRAndRFrag.bedpe
+
+if [ "$nodelete" != "nodelete" ]; then
+	rm ${samplename}/${bamname}_mappedToBaits_baitOnRight.bedpe
+        lessfilename=/dev/null
+else
+        lessfilename=${samplename}/${bamname}_mappedToBaitsBoRAndRFrag_fless06.bedpe
+fi
+
+echo "Removing reads that failed the min overhang filter..."
+awk -v fless=$lessfilename -v fmore=${samplename}/${bamname}_mappedToBaitsBoRAndRFrag_fmore06.bedpe 'BEGIN{i=0; k=0}{
+    if ($0~/\-1\t\-1/){
+         print $0 > fless;
+         i++
+    } 
+    else{
+         print $0 > fmore;
+         k++;
+    }
+    }
+END{
+    printf ("Filtered out %f reads with <60%% overlap with a single digestion fragment\n", i/(i+k));
+}' ${samplename}/${bamname}_mappedToBaitsBoRAndRFrag.bedpe
+
+if [ "$nodelete" != "nodelete" ]; then
+        rm ${samplename}/${bamname}_mappedToBaitsBoRAndRFrag.bedpe
+fi
+
+echo "Adding frag length and signed distance from bait; removing self-ligation fragments (if any; not expected with HiCUP input)..."
+perl -ne '{ 
+    chomp $_; 
+    my @a = split /\t/, $_; 
+    my $l = $a[16]-$a[15]; 
+    my $d = "NA"; 
+    if ($a[10] eq $a[14]){ 
+           my $midB = int(($a[12]+$a[11])/2+0.5); 
+           my $midR = int(($a[15]+$a[16])/2+0.5); 
+           $d = $midR - $midB; 
+    } 
+    if ($d ne "0"){ 
+           print "$_\t$l\t$d\n" 
+    }
+}' ${samplename}/${bamname}_mappedToBaitsBoRAndRFrag_fmore06.bedpe > ${samplename}/${bamname}_mappedToBaitsBoRAndRFrag_fmore06_withDistSignLen.bedpe
+
+if [ "$nodelete" != "nodelete" ]; then
+        rm ${samplename}/${bamname}_mappedToBaitsBoRAndRFrag_fmore06.bedpe
+fi
+
+
+echo "Pooling read pairs..."
+echo "##        samplename=${samplename}        bamname=${bamname}      baitmapfile=${baitfendsid}      digestfile=${digestbed}" > ${samplename}/${samplename}.chinput
+echo "baitID	otherEndID	N	otherEndLen	distSign" >> ${samplename}/${samplename}.chinput
+awk '{ 
+    if (!baitOtherEndN[$14"\t"$18]){ 
+         baitOtherEndN[$14"\t"$18] = 1; 
+         baitOtherEndInfo[$14"\t"$18] = $20"\t"$21 
+    }
+    else{ 
+         baitOtherEndN[$14"\t"$18]++; 
+    }  
+}END{ 
+    for (key in baitOtherEndN){
+         print key"\t"baitOtherEndN[key]"\t"baitOtherEndInfo[key];
+    }
+}' ${samplename}/${bamname}_mappedToBaitsBoRAndRFrag_fmore06_withDistSignLen.bedpe | sort -k1,1 -k2,2n -T ${samplename} >> ${samplename}/${samplename}.chinput
+
+if [ "$nodelete" != "nodelete" ]; then
+	rm ${samplename}/${bamname}_mappedToBaitsBoRAndRFrag_fmore06_withDistSignLen.bedpe
+fi
+
+echo "Done! The file to be used for Chicago R package input is ${samplename}/${samplename}.chinput"
---
+++ chicagoTools/makeNBaitsPerBinFile.py
@@ -0,0 +1,187 @@
+import getopt
+import sys
+import random
+import fnmatch
+import os
+
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+sys.stdout = Unbuffered(sys.stdout)
+
+maxLBrownEst = 1.5e6
+binsize = 20000
+rmapfile = ""
+baitmapfile = ""
+outfile = ""
+designDir=""
+removeAdjacent = True
+
+def usage():
+  print "Usage: python makeNBaitsPerBinFile.py [--maxLBrownEst=%d] [--binsize=%d] [--removeAdjacent=True]\n\t[--rmapfile=designDir/*.rmap]\n\t[--baitmapfile=designDir/*.baitmap]\n\t[--outfile=designDir/nbaitsperbin.nbpb]\n\t[--designDir=.]\n\nIf designDir is provided and contains a single <baitmapfile>.baitmap and <rmapfile>.rmap, these will be used unless explicitly specified.\nLikewise, the output file will be saved as designDir/nbaitsperbin.nbpb unless explicitly specified." \
+  % (maxLBrownEst, binsize)
+
+try:
+  opts, args = getopt.getopt(sys.argv[1:], 'l:b:jr:b:o:d:', \
+['maxLBrownEst=', 'binsize=', 'removeAdjacent=', 'rmapfile=', 'baitmapfile=', 'outfile=', 'designDir='])
+except getopt.GetoptError:
+  usage()
+  sys.exit(120)
+   
+for opt, arg in opts: 
+  if opt in ('--maxLBrownEst', '-l'):
+    maxLBrownEst = long(arg)
+  elif opt in ('--binsize', '-b'):
+    binsize = long(arg)
+  elif opt in ('--rmapfile', '-f'):
+    rmapfile = arg
+  elif opt in ('--baitmapfile', '-t'):
+    baitmapfile = arg
+  elif opt in ('--outfile', '-o'):
+    outfile = arg
+  elif opt in ('--designDir', '-d'):
+    designDir = arg
+  elif opt == '--removeAdjacent':
+    removeAdjacent = str2bool(arg)
+  elif opt == '-j':
+    removeAdjacent = True
+
+if designDir != "":
+  if os.path.isdir(designDir):
+    print "\nUsing designDir %s" % designDir;
+  else:
+    print "\nError: designDir does not exist.\n";
+    usage()
+    sys.exit(1)
+else:
+  designDir = "."
+
+if baitmapfile == "":
+  files = os.listdir(designDir)
+  whichFiles = []
+  for file in files:
+    if fnmatch.fnmatch(file, '*.baitmap'):
+        whichFiles.append(file)
+  if len(whichFiles)==1:
+    baitmapfile=os.path.join(designDir, whichFiles[0])
+    print "Located baitmapfile %s in %s" % (whichFiles[0], designDir)
+  else:
+    print "\nError: could not unambiguously locate baitmapfile in designDir.\n"
+    usage()
+    sys.exit(1)
+
+if rmapfile == "":
+  files = os.listdir(designDir)
+  whichFiles = []
+  for file in files:
+    if fnmatch.fnmatch(file, '*.rmap'):
+        whichFiles.append(file)
+  if len(whichFiles)==1:
+    rmapfile=os.path.join(designDir, whichFiles[0])
+    print "Located rmapfile %s in %s" % (whichFiles[0], designDir)
+  else:
+    print "\nError: could not unambiguously locate baitmapfile in designDir.\n"
+    usage()
+    sys.exit(1)
+
+
+if outfile == "":
+  outfile = os.path.join(designDir, "nbaitsperbin.nbpb")
+  print "Output fill be saved as %s\n" % outfile
+
+print "Using options:\nmaxLBrownEst=%d, binsize=%d removeAdjacent=%r\n\trmapfile=%s\n\tbaitmapfile=%s\n\toutfile=%s\n" \
+% (maxLBrownEst, binsize, removeAdjacent, rmapfile, baitmapfile, outfile)
+
+a = open(rmapfile)
+print "Reading rmap...."
+chr = []
+st = []
+end = []
+id = []
+for line in a:
+  line = line.strip()
+  l = line.split("\t")
+  chr.append(l[0])
+  st.append(int(l[1]))
+  end.append(int(l[2]))
+  id.append(int(l[3]))
+a.close()
+
+b = open(baitmapfile)
+print "Reading baitmap..."
+bid = []
+for line in b:
+  line = line.strip()
+  l = line.split("\t")
+  bid.append(int(l[3]))
+b.close()
+
+bid = set(bid)
+
+print "Sorting rmap..."
+
+oldchr = chr
+oldst = st
+chr = [x for (x,y) in sorted(zip(oldchr, oldst))]
+st = [y for (x,y) in sorted(zip(oldchr, oldst))]
+end = [z for (x,y,z) in sorted(zip(oldchr, oldst, end))]
+id = [z for (x,y,z) in sorted(zip(oldchr, oldst, id))]
+del oldchr
+del oldst
+
+print "Looping through other ends..."
+
+of = open(outfile, "wt")
+of.write("#\tmaxLBrownEst=%d\tbinsize=%d\trmapfile=%s\tbaitmapfile=%s\n" % (maxLBrownEst, binsize, rmapfile, baitmapfile))
+
+for i in xrange(len(st)):
+    
+  n = [0]*int(maxLBrownEst/binsize)
+  
+  if removeAdjacent:
+    iSt=i-2
+  else:
+    iSt=i-1
+
+  for j in xrange(iSt,0,-1):
+   if chr[j] != chr[i]:
+    break
+   d = st[i]+(end[i]-st[i])/2-(st[j]+(end[j]-st[j])/2)
+   if d>=maxLBrownEst:
+    break
+   if id[j] in bid:
+    n[d/binsize] += 1
+  
+  if removeAdjacent:
+    iSt=i+2
+  else:
+    iSt=i+1
+
+  for j in xrange(iSt,len(st),1):
+   if chr[j] != chr[i]:
+    break
+   d = st[j]+(end[j]-st[j])/2-(st[i]+(end[i]-st[i])/2)
+   if d>=maxLBrownEst:
+    break
+   if id[j] in bid:
+    n[d/binsize] += 1
+  
+  of.write("%d\t" % id[i])
+  for k in xrange(len(n)):
+    of.write("%d" % n[k])
+    if k!=len(n)-1:
+      of.write("\t")
+  of.write("\n")
+ 
+  if int(random.uniform(0,1000))==1: 
+   print "%d " % i,
+
+of.close()
+
+print "Done!"
