2275dbdd6f10f634c49a607886d6f4bd5173a452
Jonathan Cairns <jonathan.cairns@babraham.ac.uk>
2015-06-24 15:04:09 +0100

    vignette switched to human GM data

--- Chicago/inst/doc/Chicago.R
+++ Chicago/inst/doc/Chicago.R
@@ -6,33 +6,34 @@ library(PCHiCdata)
 
 ## ------------------------------------------------------------------------
 dataPath <- system.file("extdata", package="PCHiCdata")
-testDesignDir <- file.path(dataPath, "mm9TestDesign")
+testDesignDir <- file.path(dataPath, "hg19TestDesign")
 dir(testDesignDir)
 
 ## ------------------------------------------------------------------------
-testDataPath <- file.path(dataPath, "mESCchinputFiles")
+testDataPath <- file.path(dataPath, "GMchinputFiles")
 dir(testDataPath)
 
 files <- c(
-    file.path(testDataPath, "mESCrep1.chinput"),
-    file.path(testDataPath, "mESCrep2.chinput")
+    file.path(testDataPath, "GM_rep1.chinput"),
+    file.path(testDataPath, "GM_rep2.chinput"),
+    file.path(testDataPath, "GM_rep3.chinput")
   )
 
 ## ------------------------------------------------------------------------
-settingsFile <- file.path(system.file("extdata", package="PCHiCdata"), "smESCSettings", "smESC.settingsFile")
+settingsFile <- file.path(system.file("extdata", package="PCHiCdata"), "sGM12878Settings", "sGM12878.settingsFile")
 
-## ----, message=FALSE-----------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 library(Chicago)
 
 cd <- setExperiment(designDir = testDesignDir, settingsFile = settingsFile)
 
-## ----, message=FALSE-----------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 cd <- readAndMerge(files=files, cd=cd)
 
-## ----, eval=FALSE--------------------------------------------------------
+## ---- eval=FALSE---------------------------------------------------------
 #  cd <- chicagoPipeline(cd)
 
-## ----, echo=FALSE, message=FALSE-----------------------------------------
+## ---- echo=FALSE, message=FALSE------------------------------------------
 cd <- chicagoPipeline(cd)
 
 ## ------------------------------------------------------------------------
@@ -40,16 +41,16 @@ outputDirectory <- tempdir()
 exportResults(cd, file.path(outputDirectory, "vignetteOutput"))
 
 ## ------------------------------------------------------------------------
-featuresFolder <- file.path(dataPath, "mESCfeatures")
+featuresFolder <- file.path(dataPath, "GMfeatures")
 dir(featuresFolder)
 
-featuresFile <- file.path(featuresFolder, "featuresmESC.txt")
+featuresFile <- file.path(featuresFolder, "featuresGM.txt")
 featuresTable <- read.delim(featuresFile, header=FALSE, as.is=TRUE)
 featuresList <- as.list(featuresTable$V2)
 names(featuresList) <- featuresTable$V1
 featuresList
 
-## ----, message=FALSE-----------------------------------------------------
+## ---- message=FALSE, fig.width=12, fig.height=7--------------------------
 no_bins <- ceiling(max(abs(cd@x$distSign), na.rm = T)/1e4)
 
 enrichmentResults <- peakEnrichment4Features(cd, folder=featuresFolder, list_frag=featuresList,
