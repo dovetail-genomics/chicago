12931ca84b6ac7204b9d8a2a4cb23d9b510597ce
Jonathan Cairns <jonathan.cairns@babraham.ac.uk>
2016-08-24 16:52:51 +0100

    Startup message linking to NEWS

--- Chicago/R/zzz.R
+++ Chicago/R/zzz.R
@@ -3,5 +3,6 @@
 
 	packageStartupMessage("Welcome to CHiCAGO - version ", utils::packageDescription("Chicago", fields="Version"))
 	packageStartupMessage('If you are new to CHiCAGO, please consider reading the vignette through the command: vignette("Chicago").')
-}
+	packageStartupMessage('NOTE: tlb.minProxOEPerBin and tlb.minProxB2BPerBin defaults changed as of Version 1.1.5. See news(package="Chicago")')
 
+}
